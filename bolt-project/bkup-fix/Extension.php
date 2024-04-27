b5c3a8ed2efebf7601a79e468c89d0794bef9d17
Fernando Chicaiza <washopilot@yahoo.com>
2024-04-27 01:08:43 -0500

    fix-menu-ui

---
+++ bolt-project/bkup-fix/Extension.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Eckinox\BoltNavigationUI;
+
+use Bolt\Extension\BaseExtension;
+use Symfony\Component\Routing\Route;
+
+class Extension extends BaseExtension
+{
+    public function getName(): string
+    {
+        return 'Bolt Navigation UI';
+    }
+
+    public function initialize(): void
+    {
+        $this->addTwigNamespace('bolt-navigation-ui');
+    }
+
+    public function getRoutes(): array
+    {
+        return [
+            'bolt_eckinox_navigation_list' => new Route(
+                '/bolt/navigations',
+                ['_controller' => 'Eckinox\BoltNavigationUI\Controller\ListController::list']
+            ),
+            'bolt_eckinox_navigation_save' => new Route(
+                '/bolt/navigation/save',
+                ['_controller' => 'Eckinox\BoltNavigationUI\Controller\EditController::save']
+            ),
+            'bolt_eckinox_navigation_search' => new Route(
+                '/bolt/navigation/content-search',
+                ['_controller' => 'Eckinox\BoltNavigationUI\Controller\SearchController::search']
+            ),
+            'bolt_eckinox_navigation_edit' => new Route(
+                '/bolt/navigation/{name}',
+                ['_controller' => 'Eckinox\BoltNavigationUI\Controller\EditController::edit'],
+                ['name' => '[a-zA-Z0-9_\\-]+']
+            ),
+        ];
+    }
+}
